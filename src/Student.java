e24e4cfc1f0c5c1c891d95c296c555591efdd455
Achini Nawodya <2023ict088@stu.vau.ac.lk>
2026-06-10 10:45:24 +0530

    Create Student class with encapsulation

--- src/Student.java
+++ src/Student.java
@@ -2,4 +2,23 @@ public class Student {
     String id;
     String name;
     double marks;
+	public class Student {
+    private String id;
+    private String name;
+    private double marks;
+
+    public Student(String id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public double getMarks() { return marks; }
+
+    public void displayStudent() {
+        System.out.println(id + " " + name + " " + marks);
+    }
+}
 }
